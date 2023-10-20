12a1201030d1ef060495f2aa06c0b36e38758e8f
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2023-10-20 10:08:12 +0200

    renamed vl folie, import vl erstellt

--- Prog DSKI/20232010_VL_funktionen.py
+++ Prog DSKI/20232010_VL_funktionen.py
@@ -31,15 +31,31 @@
 # globaler geltungsbereich: können von jeder Funktion aufgerufen werden!
 # 
 
-a=5
-b=9
+# a=5
+# b=9
 
-def something_to_do(a):
-    # global a # zieht sich aus dem global space in die lokale funktion, und verändert diesen
-    a = a * 2 # aus einem lokalen raum können globale variablen nicht verändert werden!
-    a = 1000 # diese variable überschreibt innerhalb der funktion den Wert für a, aber nicht global!
-    return a+b
+# def something_to_do(a):
+#     # global a # zieht sich aus dem global space in die lokale funktion, und verändert diesen
+#     a = a * 2 # aus einem lokalen raum können globale variablen nicht verändert werden!
+#     a = 1000 # diese variable überschreibt innerhalb der funktion den Wert für a, aber nicht global!
+#     return a+b
 
-result = something_to_do(a)
-print(a, b, result)
+# result = something_to_do(a)
+# print(a, b, result)
+
+# EXAMPLE 4 ------------------------------------------------------------------
+
+# returns sind exit-punkte einer Funktion
+
+def pick_smallest(a, b):
+    if a > b:
+        return b
+    elif a < b:
+        return a
+    elif a == b: # das ist überflüssig, das macht python automatisch...
+        return None
+
+
+result = pick_smallest(7, 7)
+print(result) # --> 6
 
---
+++ Prog DSKI/20232010_VL_import.py
@@ -0,0 +1,8 @@
+# allgemeine benutztung von bibs
+# Python Bibs sind einfache online verfügbarer Code
+# Typischerweise enthalten Bibs nützliche Funktionen für bestimmte Zwecke
+
+# standart bibs aus python
+import math
+
+print(math.pi)
