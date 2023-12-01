fde53acf302b187e0c6b75bec024710a41df4017
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2023-12-01 12:18:19 +0100

    made branch of main

--- Prog DSKI/Projekt/mnk_main.py
+++ Prog DSKI/Projekt/mnk_main.py
@@ -1,53 +1,6 @@
 # imports
 import numpy as np
-
-class Game:
-    
-    def __init__(self, m, n, k, board, player1, player2):
-        """_summary_:
-        Board is filled with zeros
-        player1 places 1
-        player2 places 2
-
-        Args:
-            m (_type_): _description_
-            n (_type_): _description_
-            k (_type_): _description_
-            board (_type_): _description_
-            player1 (_type_): _description_
-            player2 (_type_): _description_
-        """
-        self.m = m
-        self.n = n
-        self.k = k
-        self.board = board
-        self.player1 = player1
-        self.player2 = player2
-
-    def start():
-        """_summary_
-        """
-        pass
-    
-    def has_won():
-        """_summary_
-        playerX has won when there is a k-long Pattern on the m x n board
-        start checking for winning pattern after k moves
-        
-        checking process:
-        - pick a placed move of the player that just went
-        - check if surrounding 8 array cells have the same label
-        - if not, next player
-        - if there is an entry in a neighbor cell, follow the direction k times. if the
-              
-        """
-        pass
-    
-    def game_loop(): #eigentliches gameplay
-        """_summary_
-        """
-        pass
-
+import random
 
 class Board:
     
@@ -78,8 +31,9 @@ class Board:
             board: as numpy array size n x m
         """
         self.board = np.zeros((self.m, self.n), dtype=int)
-        return self.board
-    
+        print(self.board)
+        pass
+
     
 class Player: #ich bin noch nicht fertig!! ich habe nur laut nachgedacht
     
@@ -134,6 +88,54 @@ class MyBot(Player):
             board (_type_): _description_
         """
         pass
+
+class Game(Board):
     
+    def __init__(self, m:int, n:int, k:int, player1:str, player2:str):
+        """_summary_:
+        
+        player1 places 1
+        player2 places 2
+
+        Args:
+            m (_type_): _description_
+            n (_type_): _description_
+            k (_type_): _description_
+            board (_type_): _description_
+            player1 (_type_): _description_
+            player2 (_type_): _description_
+        """
+        self.m = m
+        self.n = n
+        self.k = k
+        self.player1 = player1
+        self.player2 = player2
+
+    def start(self):
+         
+        Board(self.m, self.n, self.k).display()
+        pass
     
-print(Board(5, 5, 4).display())
+    def has_won():
+        """_summary_
+        playerX has won when there is a k-long Pattern on the m x n board
+        start checking for winning pattern after k moves
+        
+        checking process:
+        - pick a placed move of the player that just went
+        - check if surrounding 8 array cells have the same label
+        - if not, next player
+        - if there is an entry in a neighbor cell, follow the direction k times. if the
+              
+        """
+        pass
+    
+    def game_loop(): #eigentliches gameplay
+        """_summary_
+        """
+        pass
+
+
+if __name__ == "__main__":
+
+    Game(3, 5, 1, "p1", "p2").start()
