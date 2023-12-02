41ae946ac9d1b146995dfd19cfdda86d911ff318
moodydoodi <148434600+moodydoodi@users.noreply.github.com>
2023-12-02 15:14:43 +0100

    is_valid und make_move für random bot

--- Prog DSKI/Projekt/mnk_main.py
+++ Prog DSKI/Projekt/mnk_main.py
@@ -1,53 +1,6 @@
 # imports
 import numpy as np
-
-class Game:
-    
-    def __init__(self, m, n, k, board, player1, player2):
-        """_summary_:
-        Board is filled with zeros
-        player1 places 1
-        player2 places 2
-
-        Args:
-            m (_type_): _description_
-            n (_type_): _description_
-            k (_type_): _description_
-            board (_type_): _description_
-            player1 (_type_): _description_
-            player2 (_type_): _description_
-        """
-        self.m = m
-        self.n = n
-        self.k = k
-        self.board = board
-        self.player1 = player1
-        self.player2 = player2
-
-    def start():
-        """_summary_
-        """
-        pass
-    
-    def has_won():
-        """_summary_
-        playerX has won when there is a k-long Pattern on the m x n board
-        start checking for winning pattern after k moves
-        
-        checking process:
-        - pick a placed move of the player that just went
-        - check if surrounding 8 array cells have the same label
-        - if not, next player
-        - if there is an entry in a neighbor cell, follow the direction k times. if the
-              
-        """
-        pass
-    
-    def game_loop(): #eigentliches gameplay
-        """_summary_
-        """
-        pass
-
+import random
 
 class Board:
     
@@ -120,18 +73,76 @@ class Player: #ich bin noch nicht fertig!! ich habe nur laut nachgedacht
         """
         pass
     
-class MyBot(Player):
+class MyBot_Random(Player):
     
     def __init__(self):
         """_summary_
         """
         pass
-        
+    
     def make_move(self, board): #überarbeiten im vergleich zu player
-        """_summary_k > n or self.k < self.
-
+        """_summary_k > n or self.k < self.n
         Args:
             board (_type_): _description_
+        """
+        while True:
+            x = random.randint(0, self.m - 1)
+            y = random.randint(0, self.n - 1)
+            if self.is_valid(x, y):
+                self.board[x][y] = Player() #da bin ich mir noch nicht sicher
+            return x, y
+    
+class Game(Board):
+    
+    def __init__(self, m, n, k, board, player1, player2):
+        """_summary_:
+        Board is filled with zeros
+        player1 places 1
+        player2 places 2
+
+        Args:
+            m (_type_): _description_
+            n (_type_): _description_
+            k (_type_): _description_
+            board (_type_): _description_
+            player1 (_type_): _description_
+            player2 (_type_): _description_
+        """
+        self.m = m
+        self.n = n
+        self.k = k
+        self.board = board
+        self.player1 = player1
+        self.player2 = player2
+
+    def start():
+        """_summary_
+        """
+        pass
+    
+    def has_won():
+        """_summary_
+        playerX has won when there is a k-long Pattern on the m x n board
+        start checking for winning pattern after k moves
+        
+        checking process:
+        - pick a placed move of the player that just went
+        - check if surrounding 8 array cells have the same label
+        - if not, next player
+        - if there is an entry in a neighbor cell, follow the direction k times. if the
+              
+        """
+        pass
+    
+    def is_valid(self, m, n):
+        valid_row = 0 <= m < self.m
+        valid_col = 0 <= n < self.n
+        empty_cell = self.board[m][n] == 0
+        
+        return valid_row and valid_col and empty_cell
+    
+    def game_loop(): #eigentliches gameplay
+        """_summary_
         """
         pass
     
