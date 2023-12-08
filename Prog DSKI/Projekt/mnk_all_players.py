9796440e39208c97fc81c44966bb39ab68fa1ea0
Dalia Salih <dalia.salih@study.hs-duesseldorf.de>
2023-12-08 12:46:57 +0100

    is_valid angepasst, bot make_move verändert

--- Prog DSKI/Projekt/mnk_all_players.py
+++ Prog DSKI/Projekt/mnk_all_players.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mnk_board as board
 import random
 
 
@@ -14,33 +15,39 @@ class Player():
         player_move = (int(input("Please make a move: ")), int(input("")))
         return player_move
 
-    
-    def is_valid(self, m, n, board):
-        valid_row = 0 <= m < self.m
-        valid_col = 0 <= n < self.n
-        empty_cell = self.board[m][n] == 0
-        
-        return valid_row and valid_col and empty_cell
+    #Dalia
+    def is_valid(self, moves:tuple, board):
+        '''
+        erhält ein tuple von moves
+        prüft ob moves in valid raum ist
+        gibt true or false wieder
+        '''
+        valid_row = 0 <= moves[0] < self.m
+        valid_col = 0 <= moves[1] < self.n
+        empty_cell = board[moves[0]][moves[1]] == 0
+        if valid_row and valid_col and empty_cell:
+            return True
+        return False
 
 
-class Bot_radom(Player):
+class Bot_random(Player):
 
     def __init__(self, player_number, name="random bot") -> None:
         super().__init__(player_number, name)
-
+        
     def make_move(self): # -> (row, col)
         """_summary_k > n or self.k < self.n
+        made by Dalia
         Args:
             board (_type_): _description_
         """
-        move = (1, 1)
+        moves = (1, 1)
         while True:
-            x = random.randint(0, self.m - 1)
-            y = random.randint(0, self.n - 1)
-            if self.is_valid(x, y):
-                self.board[x][y] = Player() #da bin ich mir noch nicht sicher
-            move = (x, y)
-            return move
+            moves[0] = random.randint(0, self.board.shape[0] - 1)
+            moves[1] = random.randint(0, self.board.shape[1] - 1)
+            if self.is_valid(moves):
+                self.board[moves[0]][moves[1]] = Player() #da bin ich mir noch nicht sicher - I think it's false
+            return moves
 
 
 class Bot_simple(Player):
@@ -55,7 +62,7 @@ class Bot_simple(Player):
         return move
 
 
-class Bot_comples(Player):
+class Bot_complex(Player):
 
     def __init__(self, player_number, name="complex bot") -> None:
         super().__init__(player_number, name)
