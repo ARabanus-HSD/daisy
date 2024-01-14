3f3058afcee787b51895fcb73b787e07e44c1132
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2024-01-14 21:00:53 +0100

    kisd

--- IntlSys/Finale_Abgabe_IntlSys/project/plotting.py
+++ IntlSys/Finale_Abgabe_IntlSys/project/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 data_dir = "/home/arabanus/Desktop/intlsys_logs/"
 m_1 = "logfile_deo_dose_53mm.txt"
@@ -14,9 +15,28 @@ df_cube2 = pd.read_csv(data_dir + m_3, header=None)
 df_dose2 = pd.read_csv(data_dir + m_4, header=None)
 df_dose3 = pd.read_csv(data_dir + m_5, header=None)
 
+def reshape_measurement(df, num_rot=10):
+    """
+    input pd df w/ shape: (1, n)
+    output: pd df w/ shape (num_rot, n/num_rot)
+    """
+    all_entries = df.shape[1]
+    reshaped_df = df.values.reshape(num_rot, all_entries//num_rot)
+    return reshaped_df
+
+def add_label():
+    # todo fÜr zuhause oder so
+    pass
+
+# combined_df = np.concatenate([reshape_measurement(df_dose1.T),
+                            #   reshape_measurement(df_dose2.T),
+                            #   reshape_measurement(df_dose3.T)],
+                            #   axis=0)
+
+one_rot_dose = reshape_measurement(df_dose.T)
 
 # PLOTTING
-
+"""
 fig = plt.figure(figsize=(16, 9), dpi=70)
 
 axis1 = fig.add_subplot(5, 1, 1)
@@ -53,6 +73,17 @@ axis5.plot(df_cube2, color="r")
 axis5.legend(["cube 2"])
 for i in range(9):
     axis5.axvline(x = 61 + i*61, color="g", label="rotation")
+"""
+angles = np.linspace(0, 2 * np.pi, 61, endpoint=False)
 
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 
+# ax.plot(one_rot_dose[1])
+# ax.set_rmax(2)
+# ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
+# ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
+ax.grid(True)
+ax.plot(np.append(angles, angles[0]), np.append(one_rot_dose[0], one_rot_dose[1]))
+
+ax.set_title("A line plot on a polar axis", va='bottom')
 plt.show()
